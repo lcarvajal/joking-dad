129af82c88620199034ff11b6a17a0eb02f19ce6
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-26 10:37:07 +0200

    Add displayNextJoke and displayPreviousJoke functionality

--- src/components/JokingDad.js
+++ src/components/JokingDad.js
@@ -1,29 +1,58 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import React from 'react';
 import './JokingDad.css';
 
 export default function JokingDad() {
-  const [joke, setJoke] = useState('Click the button for a new joke!');
+  const [index, setIndex] = useState(0);
+  const [jokes, setJokes] = useState([]);
 
-  function handleClick() {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'https://icanhazdadjoke.com/');
-    request.setRequestHeader('Accept', 'application/json');
-    request.onload = () => {
-      console.log(request.response)
-      if (request.status === 200) {
-        const response = JSON.parse(request.response)
-        setJoke(response.joke);
-      }
-    };
-    request.send();
+  useEffect(() => {
+    // displayNextJoke();
+  }
+  )
+
+  function displayPreviousJoke() {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  }
+
+  function displayNextJoke() {
+    if (index < jokes.length - 1) {
+      setIndex(index + 1);
+    }
+    else {
+      const request = new XMLHttpRequest();
+      request.open('GET', 'https://icanhazdadjoke.com/');
+      request.setRequestHeader('Accept', 'application/json');
+      request.onload = () => {
+        console.log(request.response)
+        if (request.status === 200) {
+          const response = JSON.parse(request.response)
+          const newJoke = response.joke;
+          setJokes([
+            ...jokes,
+            newJoke
+          ]);
+          setIndex(jokes.length);
+        }
+      };
+      request.send();
+    }
   }
 
   return (
     <div className="JokingDad">
-      <img src="./jerry-stiller.jpg" />
-      <p>{joke}</p>
-      <button onClick={handleClick}>Get Another Joke</button>
+      <div>
+        <img src="./jerry-stiller.jpg" />
+      </div>
+      <div>
+        <p>{jokes[index]}</p>
+      </div>
+      <div>
+        {jokes.length > 0 && index > 0 ? <button onClick={displayPreviousJoke}>Previous Joke</button> : null}
+        <button onClick={displayNextJoke}>Next Joke</button>
+      </div>
     </div>
   );
 }
